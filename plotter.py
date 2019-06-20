8a7885734bf9678468637019d676389b45708f72
Hideaki TAKATANI <ev18d001@steng.u-hyogo.ac.jp>
2019-06-20 18:10:41 +0900

    Load all npy files for plotting

---
+++ plotter.py
@@ -0,0 +1,59 @@
+# Proceeding向けの適当なグラフをプロット
+import numpy
+
+param_q1_init = numpy.load("param_q1_init.npy")
+param_q2_init = numpy.load("param_q2_init.npy")
+
+param_q1_opt_0 = numpy.load("param_q1_opt_0.npy")
+param_q2_opt_0 = numpy.load("param_q2_opt_0.npy")
+
+param_q1_opt_1 = numpy.load("param_q1_opt_1.npy")
+param_q2_opt_1 = numpy.load("param_q2_opt_1.npy")
+
+param_q1_opt_1E_1 = numpy.load("param_q1_opt_1E-1.npy")
+param_q2_opt_1E_1 = numpy.load("param_q2_opt_1E-1.npy")
+
+param_q1_opt_5E_1 = numpy.load("param_q1_opt_5E-1.npy")
+param_q2_opt_5E_1 = numpy.load("param_q2_opt_5E-1.npy")
+
+param_q1_opt_1E_2 = numpy.load("param_q1_opt_1E-2.npy")
+param_q2_opt_1E_2 = numpy.load("param_q2_opt_1E-2.npy")
+
+param_q1_opt_5E_2 = numpy.load("param_q1_opt_5E-2.npy")
+param_q2_opt_5E_2 = numpy.load("param_q2_opt_5E-2.npy")
+
+param_q1_opt_1E_3 = numpy.load("param_q1_opt_1E-3.npy")
+param_q2_opt_1E_3 = numpy.load("param_q2_opt_1E-3.npy")
+
+param_q1_opt_5E_3 = numpy.load("param_q1_opt_5E-3.npy")
+param_q2_opt_5E_3 = numpy.load("param_q2_opt_5E-3.npy")
+
+param_q1_opt_1E_4 = numpy.load("param_q1_opt_1E-4.npy")
+param_q2_opt_1E_4 = numpy.load("param_q2_opt_1E-4.npy")
+
+param_q1_opt_5E_4 = numpy.load("param_q1_opt_5E-4.npy")
+param_q2_opt_5E_4 = numpy.load("param_q2_opt_5E-4.npy")
+
+param_q1_opt_1E_5 = numpy.load("param_q1_opt_1E-5.npy")
+param_q2_opt_1E_5 = numpy.load("param_q2_opt_1E-5.npy")
+
+param_q1_opt_5E_5 = numpy.load("param_q1_opt_5E-5.npy")
+param_q2_opt_5E_5 = numpy.load("param_q2_opt_5E-5.npy")
+
+param_q1_opt_1E_6 = numpy.load("param_q1_opt_1E-6.npy")
+param_q2_opt_1E_6 = numpy.load("param_q2_opt_1E-6.npy")
+
+param_q1_opt_5E_6 = numpy.load("param_q1_opt_5E-6.npy")
+param_q2_opt_5E_6 = numpy.load("param_q2_opt_5E-6.npy")
+
+param_q1_opt_1E_7 = numpy.load("param_q1_opt_1E-7.npy")
+param_q2_opt_1E_7 = numpy.load("param_q2_opt_1E-7.npy")
+
+param_q1_opt_5E_7 = numpy.load("param_q1_opt_5E-7.npy")
+param_q2_opt_5E_7 = numpy.load("param_q2_opt_5E-7.npy")
+
+param_q1_opt_1E_8 = numpy.load("param_q1_opt_1E-8.npy")
+param_q2_opt_1E_8 = numpy.load("param_q2_opt_1E-8.npy")
+
+param_q1_opt_5E_8 = numpy.load("param_q1_opt_5E-8.npy")
+param_q2_opt_5E_8 = numpy.load("param_q2_opt_5E-8.npy")
